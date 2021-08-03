8841bf15b6525fbffaf3f0255b018cf2ed4c3b6c
Elizabeth Lanskaya <eli-lans@yandex.ru>
2021-08-03 20:11:18 +0300

    Add task3

--- Task3.py
+++ Task3.py
@@ -11,4 +11,4 @@ with open('sal.txt', 'r') as my_file:
         if int(i[1]) < 20000:
            poor.append(i[0])
         sal.append(i[1])
-print(f'Оклад меньше 20.000 у {poor}; средний оклад: {sum(map(int, sal)) / len(sal)}')
+print(f'Оклад меньше 20.000 -  {poor}; средний оклад: {sum(map(int, sal)) / len(sal)}')
